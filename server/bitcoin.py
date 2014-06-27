00a16151e874107432d0569e809f0bd352eda633
Sean Estey <estese@gmail.com>
2014-06-27 16:13:42 -0600

    Update bitcoin.py

--- server/bitcoin.py
+++ server/bitcoin.py
@@ -43,8 +43,10 @@ def update_markets(db, url, source):
 	import simplejson
 	import urllib2
 
-	#request = urllib2.Request('http://api.bitcoincharts.com/v1/markets.json')
-	#request = urllib2.Request('https://api.bitcoinaverage.com/ticker/all')
+	if source == 'btc_avg':
+		url = 'https://api.bitcoinaverage.com/ticker/all'
+	elif source == 'btc_charts':
+		url = 'http://api.bitcoincharts.com/v1/markets.json'
 
 	try:
 		response = urllib2.urlopen(url, timeout=1)
@@ -67,8 +69,9 @@ def update_markets(db, url, source):
 				timestamp = timestamp[:-6]
 				import calendar
 				timestamp = calendar.timegm(time.strptime(timestamp, '%a, %d %b %Y %H:%M:%S'))
-				query = {'symbol':symbol}, { '$set': {'time':timestamp, 'close':int(item[1]['last'])}}
-				db['markets'].update(query, True)
+				collection = {'symbol':symbol}
+				query = {'$set': {'time':timestamp, 'close':int(item[1]['last'])}}
+				db['markets'].update(collection, query, True)
 	
 	elif source == 'btc_charts':
 		for (i, item) in enumerate(data):
@@ -76,71 +79,57 @@ def update_markets(db, url, source):
 			if symbol in mongo_collections and item['avg'] > 0 
 			and item['close'] is not None:
 				# update symbol collection, insert new document if symbol doesn't exist (upsert)
-				query = {'symbol':symbol}, 
-					{ '$set': {'time':item['latest_trade'], 'close':int(item['close'])}}
-				db['markets'].update(query, True)
+				collection = {'symbol':symbol} 
+				query = {'$set': {'time':item['latest_trade'], 'close':int(item['close'])}}
+				db['markets'].update(collection, query, True)
 	
 	return True
 
-def read_raw_btccharts_csv(f):
-    # reads csv format: [unixtime, price, amount]
-	# returns csv format: [date (datetime) price amount]
+def parse_raw_trades(url, source):
+	# returns pandas dataframe in format: [date price amount]
+	# where date is in datetime format
 	import urllib2
     
 	try:
-		response = urllib2.urlopen(f, timeout=1)
+		response = urllib2.urlopen(url, timeout=1)
 	except urllib2.URLError, e:
-		log('URLError opening ' + f + ': ' + str(e.reason))
+		log('URLError opening ' + url + ': ' + str(e.reason))
 		return pd.DataFrame([])
 	except Exception:
-		log_exception('Exception opening ' + f)
+		log_exception('Exception opening ' + url)
 		return pd.DataFrame([])
 
-	try:
-		df = pd.read_csv(response, header=None, index_col=0, error_bad_lines=False)
-	except Exception:
-		log_exception('Could not parse CSV from ' + f)
-		return pd.DataFrame([])
+	if source == 'btc_charts':
+		# csv format: [unixtime, price, amount]
+		try:
+			df = pd.read_csv(response, header=None, index_col=0, error_bad_lines=False)
+		except Exception:
+			log_exception('Could not parse CSV from ' + url)
+			return pd.DataFrame([])
+	elif source == 'btc_avg':
+		# csv format: [datetime (YYYY-MM-DD) high low average volume] 
+		try:
+			df = pd.read_csv(response,index_col=0, error_bad_lines=False)
+		except Exception:
+			log_exception('Could not parse CSV from ' + url)
+			return pd.DataFrame([])
+			
+	if source == 'btc_charts':
+		df.columns = ['price','amount']
+		df.index = pd.to_datetime(df.index,unit='s') 
+		df.index.name = 'date'
+		return df
+	elif source == 'btc_avg':
+		df = df.drop('high',axis=1)
+		df = df.drop('low',axis=1)
+	#	df.columns = ['price', 'amount']
+		df.columns = ['price']
+		df.index = pd.to_datetime(df.index,unit='s')
+		df.index.name = 'date'
+		return df
 
-	df.columns = ['price','amount']
-	df.index = pd.to_datetime(df.index,unit='s') 
-	df.index.name = 'date'
-	return df
-
-def read_raw_btcavg_csv(f):
-	# reads csv format: [datetime (YYYY-MM-DD) high low average volume] 
-	# returns csv format: [date (datetime) price amount]
-	import urllib2
-
-	try:
-		response = urllib2.urlopen(f, timeout=1)
-	except urllib2.URLError, e:
-		log('URLError opening ' + f + ': ' + str(e.reason))
-		return pd.DataFrame([])
-	
-	try:
-		df = pd.read_csv(response,index_col=0, error_bad_lines=False)
-	except Exception:
-		log_exception('Could not parse CSV from ' + f)
-		return pd.DataFrame([])
-		
-	df = df.drop('high',axis=1)
-	df = df.drop('low',axis=1)
-#	df.columns = ['price', 'amount']
-	df.columns = ['price']
-	df.index = pd.to_datetime(df.index,unit='s')
-	df.index.name = 'date'
-	return df
-
-def create_db_collection(db, symbol, df):
-	dlist = df.to_dict('records')
-	db.create_collection(symbol)
-	db[symbol].insert(dlist)
-	db[symbol].ensure_index('date')
-	log('created collection ' + symbol + ' with ' + str(len(df)) + ' records')
-
-def process_raw_csv(df, has_volume):
-	# reads CSV from with format [datetime price amount]
+def process_raw_trades(df, has_volume):
+	# reads dataframe returned from parse_raw_trades
 	# resamples data to hourly frequency, calculates mean price with summed volume
 	if has_volume == True:
 		df = df.resample('1h', how={'price':np.mean, 'amount':np.sum})
@@ -159,6 +148,13 @@ def process_raw_csv(df, has_volume):
 		df.columns = ['date','price']
 	return df
 
+def create_db_collection(db, symbol, df):
+	dlist = df.to_dict('records')
+	db.create_collection(symbol)
+	db[symbol].insert(dlist)
+	db[symbol].ensure_index('date')
+	log('created collection ' + symbol + ' with ' + str(len(df)) + ' records')
+
 def update_trades(db, symbol, source):
 	# update latest trades since last timestamp and insert to db
 	# returns number of trades added to db (0 if error)
@@ -167,21 +163,21 @@ def update_trades(db, symbol, source):
 	
 	if source == 'btc_charts':
 		url = 'http://api.bitcoincharts.com/v1/trades.csv?symbol=' + symbol + '&start=' + str(last_timestamp)
-		df = read_raw_btccharts_csv(url)
+		df = parse_raw_trades(url, 'btc_charts')
 		if df.empty:
 			return 0
 		else:
 			db[symbol].remove({'date':last_timestamp})
-			df = process_raw_csv(df,True)
+			df = process_raw_trades(df,True)
 	elif source == 'btc_avg':
 		currency = symbol[len(symbol)-3:]
 		url = 'https://api.bitcoinaverage.com/history/' + currency + '/per_hour_monthly_sliding_window.csv'
-		df = read_raw_btcavg_csv(url)
+		df = parse_raw_trades(url, 'btc_avg')
 		if df.empty:
 			return 0
 		else:
 			db[symbol].remove({'date':last_timestamp})
-			df = process_raw_csv(df,False)
+			df = process_raw_trades(df,False)
 			current_index = df.date[df.date==last_timestamp].index[0]	
 			df = df[current_index:] # remove all records prior to last_timestamp
 
@@ -192,4 +188,3 @@ def update_trades(db, symbol, source):
 		db[symbol].insert(dlist)
 	
 	return len(df)
-
